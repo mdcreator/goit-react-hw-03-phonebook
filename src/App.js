f41d96c0a5e3aab01366dc0cbafac0432cda1431
mdcreator <63508140+mdcreator@users.noreply.github.com>
2021-03-26 23:40:54 +0200

    Add localStorage

--- src/App.js
+++ src/App.js
@@ -72,6 +72,19 @@ class App extends Component {
     );
   };
 
+  componentDidMount() {
+    const contacts = localStorage.getItem('contacts');
+    const parsedContacts = JSON.parse(contacts);
+    if (parsedContacts) {
+      this.setState({ contacts: parsedContacts });
+    }
+  }
+
+  componentDidUpdate(prevPops, prevState) {
+    if (this.state.contacts !== prevState.contacts);
+    localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+  }
+
   render() {
     const { filter } = this.state;
     const visibleFilter = this.getVisibleFilter();
